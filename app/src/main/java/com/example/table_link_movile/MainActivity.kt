4f1bfcc6fdf73a4b5da4cd14cc4dab1d73cec481
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-10-07 20:56:58 -0300

    Update README.mdasd

--- app/src/main/java/com/example/table_link_movile/MainActivity.kt
+++ app/src/main/java/com/example/table_link_movile/MainActivity.kt
@@ -1,29 +1,2 @@
 package com.example.table_link_movile
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import com.example.table_link_movile.ui.screens.MainScreen
-import com.example.table_link_movile.ui.theme.TableLinkMovileTheme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TableLinkMovileTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
-            }
-        }
-    }
-}
