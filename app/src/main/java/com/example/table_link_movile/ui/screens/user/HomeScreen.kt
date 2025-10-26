6b08c2785192851702390e9081933480572d253e
BenjaminMarin4444 <be.marinv@duocuc.cl>
2025-10-26 18:04:18 -0300

    update de homescreen

--- app/src/main/java/com/example/table_link_movile/ui/screens/user/HomeScreen.kt
+++ app/src/main/java/com/example/table_link_movile/ui/screens/user/HomeScreen.kt
@@ -1,49 +1,85 @@
 package com.example.table_link_movile.ui.screens.user
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+
+
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(){
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Text(
-                    text = "Archivo",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-
-            Button(
-                onClick = {
-
-                },
-                modifier = Modifier.width(200.dp)
-            ) {
-                Text("Seleccionar Archivo")
+    var fileName by remember{ mutableStateOf("Ningun archivo seleccionado") }
+    val filePickerLauncher = rememberLauncherForActivityResult (
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri ->
+            if ( uri != null){
+                fileName = "Archivo Seleccionado"
             }
         }
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .width(250.dp)
+                .height(140.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.surfaceVariant,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .shadow(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(12.dp)
+
+                )
+                .padding(20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Subir Archivo",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+                Button(
+                    onClick = { filePickerLauncher.launch("*/*") },
+                    modifier = Modifier
+                        .width(180.dp)
+                ) {
+                    Text("Seleccionar")
+                }
+            }
+        }
+    }
 }
 
 
